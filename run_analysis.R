7df2aa2e6fc708689f4152a84c5b2d12bd24e1d8
cwmiller21 <cwmiller21@gmail.com>
2014-05-23 06:54:07 -0400

    second commit

--- run_analysis.R
+++ run_analysis.R
@@ -62,8 +62,9 @@ setwd(old.wd)
 
 
 # find the feature names that consists of means or standard deviations
-ldf1[[2]][,3] <- with(ldf1[[2]], !(regexpr("mean", V2, ignore.case=TRUE)<0 & 
-                                     regexpr("std", V2, ignore.case=TRUE)<0 ))
+# regexpr returns a "-1" if "mean" or "std" is not found in the text
+ldf1[[2]][,3] <- with(ldf1[[2]], !(regexpr("mean", V2, ignore.case=FALSE)<0 & 
+                                     regexpr("std", V2, ignore.case=FALSE)<0 ))
 
 # features that are mean or standard deviation
 feat <- subset(ldf1[["features.txt"]], V3==TRUE)
@@ -80,9 +81,13 @@ if(dim(ldf3[["X_test.txt"]])[2] != dim(feat)[1])
 # combine the training and test data
 tidyset <- do.call(cbind, mapply(rbind, ldf2, ldf3))
 
-# replace column names to be appropriate
-#   feature names were changed to eliminate "-" and "()"
-names(tidyset) <- c("Subject", gsub(",", "", gsub("-", ".", gsub("\\(", "", gsub("\\)", "", feat[,2])))), "Activity")
+# replace column names to be appropriate tidy names
+#   feature names were changed to eliminate any "," "-" or "()"
+names(tidyset) <- c("Subject", gsub(",", "", 
+                                    gsub("-", ".", 
+                                         gsub("\\(", "",
+                                              gsub("\\)", "", 
+                                                   feat[,2])))), "Activity")
 
 # Add descriptive activity names to name the activities in the data set.
 tidyset$Activity <- with(tidyset, factor(Activity, labels=ldf1[[1]]$V2))
@@ -100,15 +105,16 @@ write.csv(tidyset, "tidyset.csv", row.names=FALSE)
 # formula <- as.formula(paste(names(tidyset)[2], " ~ Subject + Activity"))
 # aggregate(formula, data=tidyset, FUN="mean")
 
-temp <- lapply(2:87, function(xx) {
+temp <- lapply(2:(dim(tidyset)[2]-1), function(xx) {
   formula <- as.formula(paste(names(tidyset)[xx], " ~ Subject + Activity"))
   t1 <- aggregate(formula, data=tidyset, FUN="mean")
   if(xx==2) return(t1)
   return(subset(t1, select=3))
 })
 
+# create dataset by concatenate columns
 tidyset2 <- do.call(cbind, temp)
 
 # write the summary data frame tidyset2 as csv file
-write.csv(tidyset2, "tidyset2.csv", , row.names=FALSE)
+write.csv(tidyset2, "tidyset2.csv", row.names=FALSE)
 
